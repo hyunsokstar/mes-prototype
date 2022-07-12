f202f851c9e99d86ea214d666f0452f791cffe63
ohyunsok <hyunsokstar@gmail.com>
2022-07-12 17:24:01 +0900

    update for login

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,11 +1,22 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import AppLayout from "../components/AppLayout";
+import React, { useState, useEffect } from 'react';
+
 
 
 const Home: NextPage = () => {
+  function loginCheck(){
+    // console.log("hi");
+    alert("로그인 상태를 채크한뒤 만약 로그인 상태면 다시 유저 정보를 초기화");
+    console.log("get token : ", localStorage.getItem("mes-token"));
+    
+  }
+
+  useEffect(() => {
+    loginCheck();
+  }, []);
+
+
   return (
     <AppLayout>
       hello world
