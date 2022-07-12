7d50baf066b0029eefa469ca6b66e77dba39d70c
ohyunsok <hyunsokstar@gmail.com>
2022-07-12 14:33:31 +0900

    update for after login

--- front/components/LoginForm.tsx
+++ front/components/LoginForm.tsx
@@ -5,6 +5,7 @@ import api from "../utils/api"
 import userSlice from "../slices/user"
 import RenderResult from "next/dist/server/render-result";
 import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '../store/reducer';
 
 
 type Props = {}
@@ -18,6 +19,13 @@ const LoginBar = styled.h1`
 function LoginForm({ }: Props) {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+
+    const isLoggedIn = useSelector((state: RootState) => !!state.user.email);
+    const user = useSelector((state: RootState) => state.user);
+
+
+    console.log('isLoggedIn', isLoggedIn);
+
     const dispatch = useDispatch();
 
     // const handleClick = () => {
@@ -43,14 +51,14 @@ function LoginForm({ }: Props) {
             if (response.data) {
                 dispatch(
                     userSlice.actions.setUser({
-                      email: response.data.data.email,
-                      name: response.data.data.name,
-                      accessToken: response.data.data.token,
+                        email: response.data.data.email,
+                        name: response.data.data.name,
+                        accessToken: response.data.data.token,
                     }),
-                  );
+                );
             }
 
-            localStorage.setItem('mes-token' , response.data.data.token);
+            localStorage.setItem('mes-token', response.data.data.token);
 
 
 
@@ -74,23 +82,27 @@ function LoginForm({ }: Props) {
         }
     }, [email, password]);
 
-
     return (
-        <LoginBar>
-            <input
-                value={email}
-                onChange={({ target: { value } }) => setEmail(value)}
-                type="text"
-                placeholder="email"
-            />
-            <input
-                value={password}
-                onChange={({ target: { value } }) => setPassword(value)}
-                type="password"
-                placeholder="password"
-            />
-            <button onClick={handleClick}>Login</button>
-        </LoginBar>)
+        <div>
+            {isLoggedIn ? <span>{user.name} 님 안녕하세요 <button>로그 아웃</button> </span> : (
+                <LoginBar>
+                    <input
+                        value={email}
+                        onChange={({ target: { value } }) => setEmail(value)}
+                        type="text"
+                        placeholder="email"
+                    />
+                    <input
+                        value={password}
+                        onChange={({ target: { value } }) => setPassword(value)}
+                        type="password"
+                        placeholder="password"
+                    />
+                    <button onClick={handleClick}>Login</button>
+                </LoginBar>
+            )}
+        </div>
+    )
 }
 
 export default LoginForm
