9ad2a7bde28a926c9e513623ed120835f554b7db
ohyunsok <hyunsokstar@gmail.com>
2022-07-17 11:23:24 +0900

    update for text change

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -7,7 +7,7 @@ import React, { useState, useEffect } from 'react';
 const Home: NextPage = () => {
   function loginCheck(){
     // console.log("hi");
-    alert("로그인 상태를 채크한뒤 만약 로그인 상태면 다시 유저 정보를 초기화");
+    alert("로그인 상태를 채크한뒤 만약 로그인 상태면 다시 유저 정보를 초기화2");
     console.log("get token : ", localStorage.getItem("mes-token"));
     
   }
